c660347505e771eef42fc620bae720567336c9af
safoan bazine <safibazine@gmail.com>
2023-08-02 21:27:15 +0000

    first commit

---
+++ TAURI-TEST/src/App.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import GitHublogo from "./assets/GitHub.svg";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>Welcome To My App</h1>
+      <h2>Tauri is a cross-platform framework that turns JavaScript or TypeScript 
+      code into a native application for virtually any operating system</h2>
+
+      <div className="row">
+       
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://github.com/tauri-apps/tauri" target="_blank">
+          <img src={GitHublogo} className="logo GitHub" alt="GitHub logo" />
+        </a>
+      </div>
+      <p>Click On The Logo For More Info</p>
+
+      
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
